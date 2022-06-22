3018e08dc0913012e3b51b4b72c289fbcf84cbe1
Camilo Neiza <camiloneiza1@gmail.com>
2022-06-22 16:58:34 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <NavBarTop />
+    <NavBarLeft />
+    <div class="text-xs sidebar-mini-xs" :class="darkMode ? 'dark-mode' : ''">
+      <transition name="fade-slide-rigth" mode="out-in">
+        <router-view />
+      </transition>
+    </div>
+    <footer
+      class="main-footer text-xs pb-1 pt-1"
+      :class="darkMode ? 'dark-mode' : ''"
+    >
+      <strong>
+        © 2021
+        <a href="https://soscontingencias.com.co/" target="_blank">
+          SOS CONTINGENCIAS
+        </a>
+      </strong>
+      Todos los derechos reservados.
+      <div class="float-right d-sm-inline-block">
+        <a href="https://soscontingencias.com.co/" target="_blank">
+          <img src="img/logo-sos.png" alt="Logo sos" width="100" />
+        </a>
+      </div>
+    </footer>
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+  </div>
+</template>
+<script>
+import NavBarLeft from "@/components/layout/NavBarLeft.vue";
+import NavBarTop from "@/components/layout/NavBarTop.vue";
+import { ref } from "vue";
+export default {
+  name: "Home",
+  components: {
+    NavBarLeft,
+    NavBarTop,
+  },
+  setup() {
+    let darkMode = ref(false);
+
+    const darkModeChange = (dato) => {
+      console.log(dato);
+      darkMode.value = dato;
+    };
+
+    return { darkMode, darkModeChange };
+  },
+};
+</script>
+
+<style scoped>
+.fade-slide-rigth-enter-active {
+  transition: all 0.5s ease;
+}
+.fade-slide-rigth-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-slide-rigth-enter,
+.fade-slide-rigth-leave-to {
+  transform: translateX(5%);
+  opacity: 0;
+}
+.bg-sos-blue {
+  background: #00529c;
+}
+.bg-sos-green {
+  background: #b0c442;
+}
+</style>
---
+++ src/components/layout/NavBarLeft.vue
@@ -0,0 +1,102 @@
+<template>
+  <aside
+    class="main-sidebar sidebar-dark-primary elevation-4 text-xs bg-sos-blue"
+  >
+    <!-- Brand Logo -->
+    <a data-widget="pushmenu" href="#" role="button" class="brand-link">
+      <img
+        src="/img/AdminLTELogo.png"
+        alt="AdminLTE Logo"
+        class="brand-image img-circle elevation-3"
+        style="opacity: 0.8"
+      />
+      <span class="brand-text font-weight-light">Tangara</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul
+          class="nav nav-pills nav-sidebar flex-column nav-flat nav-legacy nav-compact nav-child-indent"
+          data-widget="treeview"
+          role="menu"
+          data-accordion="false"
+        >
+          <li class="nav-header">MULTI LEVEL EXAMPLE</li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-circle nav-icon"></i>
+              <p>Level 1</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-circle"></i>
+              <p>
+                Level 1
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Level 2</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                    Level 2
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="far fa-dot-circle nav-icon"></i>
+                      <p>Level 3</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="far fa-dot-circle nav-icon"></i>
+                      <p>Level 3</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="far fa-dot-circle nav-icon"></i>
+                      <p>Level 3</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Level 2</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-circle nav-icon"></i>
+              <p>Level 1</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "NavBarLeft",
+};
+</script>
